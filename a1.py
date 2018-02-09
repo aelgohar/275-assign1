b8c1d62c3030bbc8a9ac6cea9fae34bf731c51db
ahmed elgohary <amdelgohary@gmail.com>
2018-02-08 18:19:23 -0700

    added dijkstra's algo

--- a1.py
+++ a1.py
@@ -1,44 +1,7 @@
+from heapviz import *
+from graph import Graph
+from breadth_first_search import breadth_first_search
 
-def least_cost_path(graph, start, dest, cost):
-    """
-    Find and return a least cost path in graph from start vertex to dest vertex.
-    Efficiency: If E is the number of edges, the run-time is
-      O( E log(E) ).
-    Args:
-      graph (Graph): The digraph defining the edges between the
-        vertices.
-      start: The vertex where the path starts. It is assumed
-        that start is a vertex of graph.
-      dest:  The vertex where the path ends. It is assumed
-        that dest is a vertex of graph.
-      cost:  A class with a method called "distance" that takes
-        as input an edge (a pair of vertices) and returns the cost
-        of the edge. For more details, see the CostDistance class
-        description below.
-    Returns:
-      list: A potentially empty list (if no path can be found) of
-        the vertices in the graph. If there was a path, the first
-        vertex is always start, the last is always dest in the list.
-        Any two consecutive vertices correspond to some
-        edge in graph.
-        """
-        reached = {}
-def load_edmonton_graph(filename):
-    """
-    Loads the graph of Edmonton from the given file.
-    Returns two items
-      graph: the instance of the class Graph() corresponding to the
-        directed graph from edmonton-roads-2.0.1.txt
-      location: a dictionary mapping the identifier of a vertex to
-        the pair (lat, lon) of geographic coordinates for that vertex.
-        These should be integers measuring the lat/lon in 100000-ths
-        of a degree.
-    In particular, the return statement in your code should be
-      return graph, location
-    (or whatever name you use for the variables).
-    Note: the vertex identifiers should be converted to integers
-      before being added to the graph and the dictionary.
-      """
 
 class CostDistance:
     """
@@ -56,3 +19,54 @@ class CostDistance:
         Here e is a pair (u,v) of vertices.
         Returns the Euclidean distance between the two vertices u and v.
         """
+
+def least_cost_path(graph, start, dest, cost):
+    """
+    Find and return a least cost path in graph from start vertex to dest vertex.
+    Efficiency: If E is the number of edges, the run-time is
+    O( E log(E) ).
+    Args:
+    graph (Graph): The digraph defining the edges between the
+    vertices.
+    start: The vertex where the path starts. It is assumed
+    that start is a vertex of graph.
+    dest:  The vertex where the path ends. It is assumed
+    that dest is a vertex of graph.
+    cost:  A class with a method called "distance" that takes
+    as input an edge (a pair of vertices) and returns the cost
+    of the edge. For more details, see the CostDistance class
+    description below.
+    Returns:
+    list: A potentially empty list (if no path can be found) of
+    the vertices in the graph. If there was a path, the first
+    vertex is always start, the last is always dest in the list.
+    Any two consecutive vertices correspond to some
+    edge in graph.
+    """
+    reached = {}
+    events = BinaryHeap()
+    events.insert((start, start), 0)
+    while len(events) > 0:
+        pair, time = events.popmin()
+        if pair[1] != dest:
+            reached[pair[1]] = pair[0]
+            for neighbour in graph.neighbours(pair[1]):
+                events.insert((pair[1], neighbour), time + cost(v, neighbour
+    return reached
+
+def load_edmonton_graph(filename):
+    """
+    Loads the graph of Edmonton from the given file.
+    Returns two items
+    graph: the instance of the class Graph() corresponding to the
+    directed graph from edmonton-roads-2.0.1.txt
+    location: a dictionary mapping the identifier of a vertex to
+    the pair (lat, lon) of geographic coordinates for that vertex.
+    These should be integers measuring the lat/lon in 100000-ths
+    of a degree.
+    In particular, the return statement in your code should be
+    return graph, location
+    (or whatever name you use for the variables).
+    Note: the vertex identifiers should be converted to integers
+    before being added to the graph and the dictionary.
+    """
